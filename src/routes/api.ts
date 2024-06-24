4c342ff432a6285de53f9fe320582fca153e9255
juanpo12 <juanjoc2000@hotmail.com>
2024-06-24 17:40:01 -0300

    created routes without function

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,6 +1,22 @@
 import { Router } from 'express';
+import  Paths  from '../constants/Paths';
 
 const apiRouter = Router();
 
+apiRouter.get( Paths.Files.Get, (req, res) => {
+  
+});
+
+apiRouter.post( Paths.Files.Add, (req, res) => {
+  
+});
+
+apiRouter.put( Paths.Files.Update, (req, res) => {
+  
+});
+
+apiRouter.delete( Paths.Files.Delete, (req, res) => {
+  
+});
 
 export default apiRouter;
