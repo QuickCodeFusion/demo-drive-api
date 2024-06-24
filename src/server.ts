f9c57810715ce5504a52ad324a09777c11b7703d
juanpo12 <juanjoc2000@hotmail.com>
2024-06-24 15:24:07 -0300

    setup repo

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,6 @@
 
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
-import path from 'path';
 import helmet from 'helmet';
 import express, { Request, Response, NextFunction } from 'express';
 import logger from 'jet-logger';
@@ -63,28 +62,6 @@ app.use((
   }
   return res.status(status).json({ error: err.message });
 });
-
-
-// ** Front-End Content ** //
-
-// Set views directory (html)
-const viewsDir = path.join(__dirname, 'views');
-app.set('views', viewsDir);
-
-// Set static directory (js and css).
-const staticDir = path.join(__dirname, 'public');
-app.use(express.static(staticDir));
-
-// Nav to users pg by default
-app.get('/', (_: Request, res: Response) => {
-  return res.redirect('/users');
-});
-
-// Redirect to login if not logged in.
-app.get('/users', (_: Request, res: Response) => {
-  return res.sendFile('users.html', { root: viewsDir });
-});
-
 
 // **** Export default **** //
 
