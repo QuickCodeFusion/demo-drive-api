8d591b448f309ecebd9ece73067a6d2b4d6c8cb2
Uriel <urielcampos448@gmail.com>
2024-06-25 16:39:16 -0300

    added new type validation for google oauth

--- src/config/googleDrive.ts
+++ src/config/googleDrive.ts
@@ -1,15 +1,74 @@
+import fs from 'fs-extra';
+import path from 'path';
+import { authenticate } from '@google-cloud/local-auth';
 import { google } from 'googleapis';
+import { JWTInput, TJSONClient } from './types';
 
-const SCOPES = ['https://www.googleapis.com/auth/drive'];
-// import credentials from '../../credentials.json';
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
 
-const auth = new google.auth.JWT(
-  'CREDENTIALS_FILE', //   credentials.client_email,
-  undefined,
-  'CREDENTIALS_FILE', //   credentials.private_key,
-  SCOPES,
-);
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+async function loadSavedCredentialsIfExist(): Promise<TJSONClient | null> {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content.toString()) as JWTInput;
+    const client = google.auth.fromJSON(credentials);
+    return client;
+  } catch (err) {
+    return null;
+  }
+}
 
-const drive = google.drive({ version: 'v3', auth });
+/**
+ * Serializes credentials to a file compatible with GoogleAuth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client: TJSONClient | null): Promise<void> {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content.toString()) as {
+    installed?: JWTInput;
+    web?: JWTInput;
+  };
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: 'authorized_user',
+    client_id: key?.client_id,
+    client_secret: key?.client_secret,
+    refresh_token: client?.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+const drive = await authorize();
 
 export default drive;
---
+++ src/config/types.ts
@@ -0,0 +1,17 @@
+/* eslint-disable node/no-extraneous-import */
+import { JSONClient } from 'google-auth-library/build/src/auth/googleauth';
+import { OAuth2Client } from 'google-auth-library/build/src/auth/oauth2client';
+
+export interface JWTInput {
+    type?: string;
+    client_email?: string;
+    private_key?: string;
+    private_key_id?: string;
+    project_id?: string;
+    client_id?: string;
+    client_secret?: string;
+    refresh_token?: string;
+    quota_project_id?: string;
+}
+
+export type TJSONClient = JSONClient | OAuth2Client;
