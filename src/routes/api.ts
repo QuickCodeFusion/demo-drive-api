e2f52dbb19a19814fb87a25854c6248a3d4851c4
juanpo12 <juanjoc2000@hotmail.com>
2024-06-25 11:17:53 -0300

    feat[routes]:Added routes

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,22 +1,16 @@
+/* eslint-disable max-len */
 import { Router } from 'express';
 import  Paths  from '../constants/Paths';
+import FileController from '../services/fileServices';
 
 const apiRouter = Router();
 
-apiRouter.get( Paths.Files.Get, (req, res) => {
-  
-});
+apiRouter.get( Paths.Files.Get, (req, res) => FileController.getFile(req, res));
 
-apiRouter.post( Paths.Files.Add, (req, res) => {
-  
-});
+apiRouter.post( Paths.Files.Add, (req, res) => FileController.uploadFile(req, res));
 
-apiRouter.put( Paths.Files.Update, (req, res) => {
-  
-});
+apiRouter.put( Paths.Files.Update, (req, res) => FileController.updateFile(req, res));
 
-apiRouter.delete( Paths.Files.Delete, (req, res) => {
-  
-});
+apiRouter.delete( Paths.Files.Delete, (req, res) => FileController.deleteFile(req, res));
 
 export default apiRouter;
